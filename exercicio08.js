499b599d95d58bb49aefd93038e3821bf03bf834
mariaeandrade <dudadeandrade13@gmail.com>
2026-03-02 10:54:59 -0300

    finaliza exercicio 09

---
+++ exercicio08.js
@@ -0,0 +1,10 @@
+const clientes = [
+  { nome: "Lucas", email: "lucas@test.com", ativo: true },
+  { nome: "Julia", email: "julia@test.com", ativo: false },
+  { nome: "Marcos", email: "marcos@test.com", ativo: true }
+];
+const emailsAtivos = clientes
+  .filter(cliente => cliente.ativo)
+  .map(cliente => cliente.email);
+
+  console.log(emailsAtivos)
